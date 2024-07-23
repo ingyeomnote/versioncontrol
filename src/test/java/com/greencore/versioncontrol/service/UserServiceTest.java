781b0a00d58041e24b0d7667ec447f8dcec7642b
KimInKyeom <freeartjin@naver.com>
2024-07-23 22:15:00 +0900

    주석 제거

--- src/test/java/com/greencore/versioncontrol/service/UserServiceTest.java
+++ src/test/java/com/greencore/versioncontrol/service/UserServiceTest.java
@@ -7,9 +7,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
